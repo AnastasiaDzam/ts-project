c88bd92429cb89a2cdfa7e6d50535b7317233250
AnastasiaDzam <yorklo@mail.ru>
2024-11-24 18:53:21 +0300

    сделано все

--- client/src/pages/NotFound/NotFound.tsx
+++ client/src/pages/NotFound/NotFound.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import './NotFound.css'
 
 function NotFound():JSX.Element {
   return (
-    <div>
-      сделали Бух....
+    <div className="not-found">
+      <h1>Сделали Бух....</h1>
     </div>
   )
 }
